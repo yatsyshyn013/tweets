efde44f8506c1b3ef86bf01d93e1655865908ab5
Maksym Yatsyshyn <max.yatsushyn@gmail.com>
2023-05-21 01:29:12 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -7,8 +7,11 @@ function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
-      <div>
+    <div>
+      
+      <p>"hello"</p>
+      
+      {/* <div>
         <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
@@ -27,8 +30,8 @@ function App() {
       </div>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
-      </p>
-    </>
+      </p> */}
+    </div>
   )
 }
 
