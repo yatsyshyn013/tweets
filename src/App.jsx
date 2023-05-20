b32233425f1070e1efdf15f3cbc73fa644303964
Maksym Yatsyshyn <max.yatsushyn@gmail.com>
2023-05-21 01:38:47 +0300

    updt

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,15 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+// import { useState } from 'react'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  // const [count, setCount] = useState(0)
 
   return (
     <div>
       
-      <p>"hello"</p>
+      <p>Hello</p>
       
       {/* <div>
         <a href="https://vitejs.dev" target="_blank">
