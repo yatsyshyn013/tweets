0a0689ba7346e25740e035c95a7ffcca00fcbf99
Maksym Yatsyshyn <max.yatsushyn@gmail.com>
2023-05-22 03:13:13 +0300

    updt

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter basename={"/REPO-NAME"}>
+    <BrowserRouter basename={"/tweets"}>
       <App />
     </BrowserRouter>
   </React.StrictMode>,
